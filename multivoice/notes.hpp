3f441e29f9cfad47ffb6fe8de4ccdc46ae2ec0c5
Nicolas Gry <grybouilli@outlook.fr>
2024-10-06 19:23:20 +0200

    [notes] defines for notes frequencies

---
+++ multivoice/notes.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#define C4 262
+#define D4 294
+#define E4 330
+#define F4 349
+#define G4 392
+#define A4 440
+#define B4 494
+#define C5 523
+#define D5 587
+#define E5 659
+#define F5 698
+#define G5 784
+#define A5 880
+#define B5 988
+#define C6 1047
+#define D6 1175
+#define E6 1319
+#define F6 1397
+#define G6 1568
+#define A6 1760
+#define B6 1976
